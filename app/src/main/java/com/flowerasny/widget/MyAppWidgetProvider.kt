fd8b9616bf690ee31e04765b575189e42d41bdfb
flowerasny <kuba@withplum.com>
2022-09-12 20:46:12 +0200

    Fix refresh button

--- app/src/main/java/com/flowerasny/widget/MyAppWidgetProvider.kt
+++ app/src/main/java/com/flowerasny/widget/MyAppWidgetProvider.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetManager.EXTRA_APPWIDGET_ID
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -12,6 +14,7 @@ import android.util.Log
 import android.widget.RemoteViews
 import android.widget.Toast
 
+const val REFRESH_ACTION = "com.flowerasny.widget.REFRESH_ACTION"
 const val TOAST_ACTION = "com.example.android.stackwidget.TOAST_ACTION"
 const val EXTRA_ITEM = "com.example.android.stackwidget.EXTRA_ITEM"
 
@@ -22,7 +25,7 @@ class MyAppWidgetProvider : AppWidgetProvider() {
         Log.d("WidgetTag", "update")
         appWidgetIds.forEach { widgetId ->
             val intent = Intent(context, MyAppWidgetService::class.java).apply {
-                putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId)
+                putExtra(EXTRA_APPWIDGET_ID, widgetId)
                 data = Uri.parse(toUri(Intent.URI_INTENT_SCHEME))
             }
             val views = RemoteViews(context.packageName, R.layout.widget_content).apply {
@@ -31,12 +34,24 @@ class MyAppWidgetProvider : AppWidgetProvider() {
                 setEmptyView(R.id.lvItems, R.id.tvEmpty)
             }
 
+            val refreshPendingIntent: PendingIntent = Intent(
+                context,
+                MyAppWidgetProvider::class.java
+            ).run {
+                action = REFRESH_ACTION
+                putExtra(EXTRA_APPWIDGET_ID, widgetId)
+                data = Uri.parse(toUri(Intent.URI_INTENT_SCHEME))
+
+                PendingIntent.getBroadcast(context, 0, this, FLAG_UPDATE_CURRENT)
+            }
+            views.setOnClickPendingIntent(R.id.btnRefresh, refreshPendingIntent)
+
             val toastPendingIntent: PendingIntent = Intent(
                 context,
                 MyAppWidgetProvider::class.java
             ).run {
                 action = TOAST_ACTION
-                putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId)
+                putExtra(EXTRA_APPWIDGET_ID, widgetId)
                 data = Uri.parse(toUri(Intent.URI_INTENT_SCHEME))
 
                 PendingIntent.getBroadcast(context, 0, this, FLAG_UPDATE_CURRENT)
@@ -49,8 +64,11 @@ class MyAppWidgetProvider : AppWidgetProvider() {
     }
 
     override fun onReceive(context: Context, intent: Intent) {
-        //val mgr: AppWidgetManager = AppWidgetManager.getInstance(context)
-        if (intent.action == TOAST_ACTION) {
+        if (intent.action == REFRESH_ACTION) {
+            val widgetManager: AppWidgetManager = AppWidgetManager.getInstance(context)
+            widgetManager.getAppWidgetIds(ComponentName(context, MyAppWidgetProvider::class.java))
+                .forEach { widgetManager.notifyAppWidgetViewDataChanged(it, R.id.lvItems) }
+        } else if (intent.action == TOAST_ACTION) {
             /*val widgetId: Int = intent.getIntExtra(
                 AppWidgetManager.EXTRA_APPWIDGET_ID,
                 AppWidgetManager.INVALID_APPWIDGET_ID
